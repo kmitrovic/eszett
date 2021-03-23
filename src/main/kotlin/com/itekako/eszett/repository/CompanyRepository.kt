0608744466fbece8b7f349aea4181cb03736697e
Kristijan Mitrovic <kmitrovic@itekako.com>
2021-03-23 14:28:32 +0100

    Add API call for fetching average salary for company

--- src/main/kotlin/com/itekako/eszett/repository/CompanyRepository.kt
+++ src/main/kotlin/com/itekako/eszett/repository/CompanyRepository.kt
@@ -1,8 +1,16 @@
 package com.itekako.eszett.repository
 
 import com.itekako.eszett.model.Company
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.rest.core.annotation.RestResource
 import org.springframework.stereotype.Repository
+import org.springframework.web.bind.annotation.PathVariable
 
 @Repository
-interface CompanyRepository : PagingAndSortingRepository<Company, Long>
+interface CompanyRepository : PagingAndSortingRepository<Company, Long> {
+
+    @RestResource(exported = false)
+    @Query("SELECT AVG(salary) FROM employee WHERE company = :companyId")
+    fun averageSalaryById(companyId: Long): Double
+}
--- src/test/kotlin/com/itekako/eszett/EszettApplicationTests.kt
+++ src/test/kotlin/com/itekako/eszett/EszettApplicationTests.kt
@@ -1,13 +1,13 @@
 package com.itekako.eszett
 
-import com.itekako.eszett.controller.RestController
+import com.itekako.eszett.controller.CompanyController
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import kotlin.test.assertNotNull
 
 @SpringBootTest
-class EszettApplicationTests(@Autowired val controller: RestController) {
+class EszettApplicationTests(@Autowired val controller: CompanyController) {
 
 	@Test
 	fun `context loads`() = assertNotNull(controller)
