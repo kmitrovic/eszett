8b4b607b6e791e4295c5c29b140d68a870a894c8
Kristijan Mitrovic <kmitrovic@itekako.com>
2021-03-25 00:39:02 +0100

    Remove not used imports

--- src/main/kotlin/com/itekako/eszett/security/Config.kt
+++ src/main/kotlin/com/itekako/eszett/security/Config.kt
@@ -1,17 +1,12 @@
 package com.itekako.eszett.security
 
-import com.itekako.eszett.security.EmployeeDetailsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler
 
 @Configuration
