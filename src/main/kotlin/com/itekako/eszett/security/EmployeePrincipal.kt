72cdb713f52cc6df995bbda8e9fcc609af52c96a
Kristijan Mitrovic <kmitrovic@itekako.com>
2021-03-25 15:32:00 +0100

    Remove not used import

--- src/main/kotlin/com/itekako/eszett/security/EmployeePrincipal.kt
+++ src/main/kotlin/com/itekako/eszett/security/EmployeePrincipal.kt
@@ -1,7 +1,6 @@
 package com.itekako.eszett.security
 
 import com.itekako.eszett.model.Employee
-import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
