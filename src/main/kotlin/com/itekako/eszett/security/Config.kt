d1c32cb16b7c55a2ba4d35abdf4bf03af0eb24ab
Kristijan Mitrovic <kmitrovic@itekako.com>
2021-03-25 13:38:52 +0100

    Simplify authorization rules

--- src/main/kotlin/com/itekako/eszett/security/Config.kt
+++ src/main/kotlin/com/itekako/eszett/security/Config.kt
@@ -2,12 +2,10 @@ package com.itekako.eszett.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler
 import org.springframework.context.support.beans
 
 @Configuration
@@ -17,19 +15,15 @@ class Config(@Suppress("UNUSED_PARAMETER") private val employeeDetailsService: E
 
     val beans = beans {
         bean<BCryptPasswordEncoder>("encoder")
-        bean("roleHierarchy") { RoleHierarchyImpl().apply { setHierarchy("ROLE_SUPERUSER > ROLE_ADMIN") } }
     }
 
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
-                .expressionHandler(DefaultWebSecurityExpressionHandler().apply { setRoleHierarchy(roleHierarchy()) })
-                .antMatchers("/", "/explorer/**").hasRole("ADMIN") // SUPERUSER only - ADMIN juts for testing
-                // only superuser creates and deletes companies
-                .antMatchers(HttpMethod.POST, "/companies/**").hasRole("SUPERUSER")
-                .antMatchers(HttpMethod.DELETE, "/companies/**").hasRole("SUPERUSER")
-                // other custom API call are either for superuser or just for admin of the specified company
-                .antMatchers("/employees/**", "/companies/**").hasRole("ADMIN")
-                // everything else (except for login/logout defined in the following lines) - just for superuser
+                // only a superuser can delete a company, and it can delete any company (even his own!)
+                .antMatchers(HttpMethod.DELETE,"/companies/*").hasRole("SUPERUSER")
+                .antMatchers("/companies/{id}/**")
+                    .access("hasRole('SUPERUSER')" +
+                            " or (hasRole('ADMIN') and principal.getCompanyId().toString() == #id)")
                 .anyRequest().hasRole("SUPERUSER")
             .and().formLogin().permitAll()
             .and().logout().permitAll()
