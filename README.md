97249dba8091a7dcc5633be45b240a2d743df35a
Kristijan Mitrovic <kmitrovic@itekako.com>
2021-03-26 22:54:33 +0100

    Add initial data migration

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Check the default UI at http://127.0.0.1:8080/ on your machine.
 
 Stop composer by interrupting it with Ctrl+c, and then execute:
 `make down`
+
+# Initial data
+Check src/main/resources/db/migration/V2021.03.26.22.41.00__Add_initial_dummy_data.sql
+
+After it is loaded, there will be a single superuser kris/kris01. No admin users will be created.
---
+++ src/main/resources/db/migration/V2021.03.26.22.41.00__Add_initial_dummy_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO company (id, name) VALUES
+  (nextval('company_id_seq'), 'EBF'),
+  (nextval('company_id_seq'), 'Itekako'),
+  (nextval('company_id_seq'), 'empty');
+
+INSERT INTO employee (id, username, password, name, surname, email, salary, address, company_id, is_superuser) VALUES
+  -- Company EBF (2 regular users):
+  (nextval('employee_id_seq'), NULL, NULL,
+    'John', 'Doe', 'john@example.com', 1500, NULL, (SELECT id FROM company WHERE name = 'EBF'), FALSE),
+  (nextval('employee_id_seq'), NULL, NULL,
+    'Jane', 'Doe', 'jane@example.com', 777.77, NULL, (SELECT id FROM company WHERE name = 'EBF'), FALSE),
+  -- Company Itekako (single superuser):
+  (nextval('employee_id_seq'), 'kris', '$2y$12$7QPF0t0fyivu3G5/vK0Rue5SOpHa5F0AU4g1CXCxQp50VVeWIVvvG',  -- pass: kris01
+    'Kristijan', 'Mitrovic', 'km@example.com', 1000, NULL, (SELECT id FROM company WHERE name = 'Itekako'), TRUE);
